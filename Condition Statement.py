83983f9b402b870552cf15554ddbf5a87767e1ff
joshnorris33 <79335902+joshnorris33@users.noreply.github.com>
2021-02-22 16:26:23 -0600

    Add files via upload

---
+++ Condition Statement.py
@@ -0,0 +1,13 @@
+name = "John"
+allowedNames = ["John","Joe","Jack" ]
+age = 10
+if name == "John" and age == 10:
+    print("Welcome John!")
+if name in allowedNames: "Welcome John!"
+
+password = input ("What is the password? ")
+
+if(password == "123"):
+    print ("Welcome, you got the password correct!")
+else:
+    print("Sorry, you got the password wrong!")
