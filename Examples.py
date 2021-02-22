1540bb1de26d50b448b681936457baf819553006
joshnorris33 <79335902+joshnorris33@users.noreply.github.com>
2021-02-22 12:04:54 -0600

    Add files via upload

---
+++ Examples.py
@@ -0,0 +1,6 @@
+first = input("what is your first name? ")
+middle = input ("what is your middle name? ")
+last = input ("What is your last name? ")
+
+full_name = first+ " "+middle+ " "+last
+print("Your full name is "+full_name+".")
